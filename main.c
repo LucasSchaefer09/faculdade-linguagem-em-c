01b65804cf2eefed32a6ee1823e699e88d946816
Lucas Schaefer <77746855+LucasSchaefer09@users.noreply.github.com>
2024-09-10 13:13:21 -0300

    .
    .

---
+++ main.c
@@ -0,0 +1,21 @@
+int main() {
+    float num1, num2, num3, media;
+
+    // Solicita ao usuário que insira três números
+    printf("Digite o primeiro numero por favor: ");
+    scanf("%f", &num1);
+
+    printf("Digite o segundo numero: ");
+    scanf("%f", &num2);
+
+    printf("Digite o terceiro numero: ");
+    scanf("%f", &num3);
+
+    // Calcula a média dos três números
+    media = (num1 + num2 + num3) / 3;
+
+    // Exibe a média calculada
+    printf("A media dos tres números e: %.2f\n", media);
+
+    return 0;
+}
